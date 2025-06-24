70d63d20c69cd9fd0daf40e65de164caa284e84a
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-06-24 17:08:41 +0500

    fixed imports and removed unnecessary code

--- FoodOrdering/src/app/(tabs)/menu/index.tsx
+++ FoodOrdering/src/app/(tabs)/menu/index.tsx
@@ -2,15 +2,10 @@ import { StyleSheet, View, Text, Image, FlatList } from "react-native";
 
 import products from "@/assets/data/products";
 import Colors from "@/src/constants/Colors";
-import ProductListItem from "@/src/components/ProductListItem";
+import ProductListItem from "@components/ProductListItem";
 
-const product = products[0];
 export default function TabOneScreen() {
   return (
-    // <View style={styles.container}>
-    //   <ProductListItem product={products[0]} />
-    //   <ProductListItem product={products[1]} />
-    // </View>
     <FlatList
       data={products}
       renderItem={({ item }) => <ProductListItem product={item} />}

6e5cca2ce6b9cf35c88557dd8a41904d6ec6c4ae
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-06-24 13:02:49 +0500

    component displaying the product's image, title, price

--- FoodOrdering/src/components/ProductListItem.tsx
+++ FoodOrdering/src/components/ProductListItem.tsx
@@ -1,18 +1,28 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, Image, Pressable } from "react-native";
 import Colors from "../constants/Colors";
 import { Product } from "../app/types";
+import { Link } from "expo-router";
+
+const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png";
 
 type ProductListItemProps = {
   product: Product;
 };
 const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
+    <Link href={`/menu/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{ uri: product.image || defaultPizzaImage }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </Pressable>
+    </Link>
   );
 };
 
@@ -20,11 +30,14 @@ export default ProductListItem;
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: Colors.light.background,
     padding: 20,
+    borderRadius: 10,
+    maxWidth: "48%",
   },
   title: {
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: "bold",
   },
   price: {

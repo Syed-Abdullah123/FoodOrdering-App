b44811390c2aff63549a6a193707363ff6f1d952
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-06-25 16:13:04 +0500

    added shopping cart icon at the right of header

--- FoodOrdering/src/app/(tabs)/menu/_layout.tsx
+++ FoodOrdering/src/app/(tabs)/menu/_layout.tsx
@@ -1,9 +1,33 @@
+import { Link } from "expo-router";
+import { Pressable } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import Colors from "@/src/constants/Colors";
 import { Stack } from "expo-router";
 
 export default function MenuStack() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Menu" }} />
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Menu",
+          headerRight: () => (
+            <Link href="/cart" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="shopping-cart"
+                    size={25}
+                    color={Colors.light.text}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+          presentation: "modal",
+        }}
+      />
     </Stack>
   );
 }

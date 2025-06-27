308d60800cf1d1f9314bddda5928171b41f3c723
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-06-27 15:32:10 +0500

    screen for admin to create a new product

---
+++ FoodOrdering/src/app/(admin)/menu/create.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Image } from "react-native";
+
+const CreateProductScreen = () => {
+  return (
+    <View>
+      <Text>CreateProductScreen</Text>
+    </View>
+  );
+};
+
+export default CreateProductScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});

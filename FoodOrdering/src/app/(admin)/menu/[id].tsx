58233c32f27d55545a3f05967b66d15e61dd128c
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-06-30 14:19:50 +0500

    shifted the header right update button here

--- FoodOrdering/src/app/(admin)/menu/[id].tsx
+++ FoodOrdering/src/app/(admin)/menu/[id].tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
-import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import { Link, Stack, useLocalSearchParams, useRouter } from "expo-router";
 import products from "@/assets/data/products";
 import { defaultPizzaImage } from "@/src/components/ProductListItem";
 import Button from "@components/Button";
 import { useCart } from "@/src/providers/CartProvider";
 import { PizzaSize } from "@/src/types/types";
+import { FontAwesome } from "@expo/vector-icons";
+import Colors from "@/src/constants/Colors";
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
@@ -32,6 +34,27 @@ const ProductDetailsScreen = () => {
 
   return (
     <View style={styles.container}>
+      <Stack.Screen
+        options={{
+          title: "Menu",
+          headerRight: () => (
+            <Link href={`/(admin)/menu/create?id=${id}`} asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="pencil-square-o"
+                    size={25}
+                    color={Colors.light.text}
+                    style={{ opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+          presentation: "modal",
+        }}
+      />
+
       <Stack.Screen options={{ title: product.name }} />
 
       <Image

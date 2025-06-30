e44404a9a1d4ad09144cc61912f9913e9092af07
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-06-30 18:26:55 +0500

    setting up basic orders screens

---
+++ FoodOrdering/src/app/(user)/orders/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import OrderListItem from "@/src/components/OrderListItem";
+import { Redirect } from "expo-router";
+
+const OrdersScreen = () => {
+  return <OrderListItem />;
+};
+
+export default OrdersScreen;
